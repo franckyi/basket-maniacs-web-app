b8ad6eb1787a713aab3e35f46f4ccd475bdf61b1
francky <edia.francky@gmail.com>
2022-08-08 00:30:43 +0200

    replacing custom elements with material components

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
-
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [],
   exports: [
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
