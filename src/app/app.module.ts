117141335d168e452af3b6e8d13f90dad931ef45
francky <edia.francky@gmail.com>
2022-08-07 10:30:08 +0200

    organize in subfolders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LatestResultsComponent } from './latest-results/latest-results.component';
-import { HomeComponent } from './home/home.component';
-import { PlayerOfDayComponent } from './player-of-day/player-of-day.component';
-import { HeaderHomeComponent } from './header-home/header-home.component';
+import { LatestResultsComponent } from './components/latest-results/latest-results.component';
+import { HomeComponent } from './views/home/home.component';
+import { PlayerOfDayComponent } from './components/player-of-day/player-of-day.component';
+import { HeaderHomeComponent } from './components/header-home/header-home.component';
 
 @NgModule({
   declarations: [
