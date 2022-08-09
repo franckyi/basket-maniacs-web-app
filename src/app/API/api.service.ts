a6f79121696e01a510dd5d7400391b48a7c6a574
francky <edia.francky@gmail.com>
2022-08-09 15:52:33 +0200

    setup api for games wip

---
+++ src/app/API/api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GamesResponse } from './games-response';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  getGames() {
+    return this.httpClient.get<GamesResponse>('https://free-nba.p.rapidapi.com/games'), {
+      headers: {
+        'X-RapidAPI-Key': 'c9cbb3c9e7msh9aa61fe6c842aa3p16bcf1jsnb868d2788b63'
+      }
+    }
+  }
+}
