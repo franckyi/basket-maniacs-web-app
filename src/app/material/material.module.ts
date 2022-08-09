94faac6d981ac459e32f3e6d948af56376024137
francky <edia.francky@gmail.com>
2022-08-09 11:47:31 +0200

    latest results wip

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+// import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [],
@@ -11,6 +12,7 @@ import {MatButtonModule} from '@angular/material/button';
     MatIconModule,
     MatCardModule,
     MatButtonModule,
+    // MatTableModule,
   ]
 })
 export class MaterialModule { }
