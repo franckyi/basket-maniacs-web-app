b314a9e2ba24124aa0239fbbdcfa9de5cf808ca2
francky <edia.francky@gmail.com>
2022-08-21 17:40:54 +0200

    setup stats - in progress

---
+++ src/app/API/stats.ts
@@ -0,0 +1,29 @@
+import { Game } from "./Game";
+import { Team } from "./Team";
+import { Player } from "./players";
+
+export interface Stats {
+    "id": number;
+    "ast": number;
+    "blk": number;
+    "dreb": number
+    "fg3_pct": number
+    "fg3a": number
+    "fg3m": number
+    "fg_pct": number
+    "fga": number
+    "fgm": number
+    "ft_pct": number
+    "fta": number
+    "ftm": number
+    "game": Game;
+    "min": string;
+    "oreb": number
+    "pf": number
+    "player": Player;
+    "pts": number
+    "reb": number
+    "stl": number
+    "team": Team;
+    "turnover": number
+}
