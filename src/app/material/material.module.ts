4fde6fec386c2f15fc637774d5d941a0141a7c76
francky <edia.francky@gmail.com>
2022-08-26 18:06:53 +0200

    searchi view - in progress

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,7 +4,9 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
-// import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [],
@@ -14,7 +16,9 @@ import {MatDividerModule} from '@angular/material/divider';
     MatCardModule,
     MatButtonModule,
     MatDividerModule,
-    // MatTableModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
   ]
 })
 export class MaterialModule { }
