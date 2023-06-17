a7d2dd93eefdba103d6ff2c27e4cd6bcf9865b8f
debonius <francky@tuta.io>
2023-06-17 11:59:09 +0200

    fix game search

--- src/app/views/search/search-game/search-game.component.ts
+++ src/app/views/search/search-game/search-game.component.ts
@@ -5,30 +5,46 @@ import { ApiService } from 'src/app/API/api.service';
   selector: 'app-search-game',
   template: `
     <div class="search">
-    <div class="d-flex">
+      <div class="d-flex">
         <mat-form-field style="margin-left: 15px;">
           <mat-label>Home team</mat-label>
-          <input [(ngModel)]="homeTeam" matInput placeholder="Team name">
+          <input
+            matInput placeholder="Team name"
+            [(ngModel)]="homeTeam"
+            (input)="resetPrevSearch()"
+          >
         </mat-form-field>
         <mat-form-field style="margin-left: 15px;">
           <mat-label>Visitor team</mat-label>
-          <input [(ngModel)]="visitorTeam" matInput placeholder="Team name">
+          <input
+            matInput placeholder="Team name"
+            [(ngModel)]="visitorTeam"
+            (input)="resetPrevSearch()"
+          >
         </mat-form-field>
       </div>
       <mat-form-field style="margin-left: 15px;">
         <mat-label>Season</mat-label>
-        <input [(ngModel)]="season" matInput placeholder="YYYY" required="required">
+        <input
+          matInput placeholder="YYYY" required="required"
+          [(ngModel)]="season"
+          (input)="resetPrevSearch()"
+        >
       </mat-form-field>
       <button mat-stroked-button class="btn-reset" color="basic" (click)="resetFilters()">Reset</button>
-      <button (click)="passQueries(season, homeTeam, visitorTeam )" [style.margin-left.px]="10" mat-flat-button color="primary">Search</button>
-      <mat-card *ngIf="results !== null && season !== '' " class="search-results">
-          <mat-card-content>
-            <ul>
-              <li *ngFor="let result of results">
-              <div>{{ result.home_team.full_name }} VS {{ result.visitor_team.full_name }} {{ result.season }}</div>
-              </li>
+      <button
+        mat-flat-button color="primary" [style.margin-left.px]="10"
+        (click)="passQueries(season, homeTeam, visitorTeam )"
+      >Search</button>
+      <p *ngIf="notFoundMsg !== '' ">{{ notFoundMsg }}</p>
+      <mat-card *ngIf="btnClicked && results !== null && season !== '' " class="search-results">
+        <mat-card-content>
+          <ul>
+            <li *ngFor="let result of results">
+            <div>{{ result.home_team.full_name }} VS {{ result.visitor_team.full_name }} {{ result.season }}</div>
+            </li>
           </ul>
-        </mat-card-content>
+      </mat-card-content>
       </mat-card>
     </div>
   `,
@@ -39,34 +55,79 @@ export class SearchGameComponent implements OnInit {
   season: string = '';
   homeTeam: string = '';
   visitorTeam: string = '';
+  notFoundMsg: string = '';
+  btnClicked: boolean = false;
 
   constructor(private _api: ApiService) {}
   ngOnInit() {}
 
+  resetBtnClicked() {
+    this.btnClicked = false;
+  }
+
   passQueries(season: string, homeTeam: string, visitorTeam: string) {
+    this.btnClicked = true;
     this.results = '';
-    if ( homeTeam !== '' && visitorTeam === '' && season !== '' ) {
+    if ( homeTeam !== '' && visitorTeam === '' ) {
       this._api.getGames(season, 500)
       .subscribe(
         (response) => {
           this.results = response.data.filter( game => 
             game.home_team.full_name.toLowerCase().includes( homeTeam.toLowerCase() )
           )
-          console.log('✅ filtered games', this.results);
+          if (this.results.length >= 1) {
+            console.log('✅ filtered games', this.results);
+          } else {
+            console.log(this.results.length);
+            console.log('not found')
+            this.notFoundMsg = 'No teams found... Try again';
+            this.results = null;
+          }
         }
       );
     } 
-    else if ( homeTeam === '' && visitorTeam !== '' && season !== '') {
+    else if ( homeTeam === '' && visitorTeam !== '' ) {
       this._api.getGames(season, 500)
       .subscribe(
         (response) => {
           this.results = response.data.filter( game => 
             game.visitor_team.full_name.toLowerCase().includes( visitorTeam.toLowerCase() )
           )
-          console.log('✅ filtered games', this.results);
+          if (this.results.length >= 1) {
+            console.log('✅ filtered games', this.results);
+          } else {
+            console.log(this.results.length);
+            console.log('not found')
+            this.notFoundMsg = 'No games found... Try again';
+            this.results = null;
+          }
         }
       );
     }
+    if ( homeTeam !== '' && visitorTeam !== '' ) {
+      this._api.getGames(season, 500)
+      .subscribe(
+        (response) => {
+          this.results = response.data.filter( game => 
+            game.home_team.full_name.toLowerCase().includes( homeTeam.toLowerCase() ) &&
+            game.visitor_team.full_name.toLowerCase().includes( visitorTeam.toLowerCase() )
+          )
+          if (this.results.length >= 1) {
+            console.log('✅ filtered games', this.results);
+          } else {
+            console.log(this.results.length);
+            console.log('not found')
+            this.notFoundMsg = 'No teams found... Try again';
+            this.results = null;
+          }
+        }
+      );
+    } 
+  }
+  
+  resetPrevSearch() {
+    this.btnClicked = false;
+    this.notFoundMsg = '';
   }
 
   resetFilters() {
@@ -74,6 +135,7 @@ export class SearchGameComponent implements OnInit {
     this.homeTeam = '';
     this.visitorTeam = '';
     this.results = null;
+    this.resetPrevSearch();
   }
 
 }
