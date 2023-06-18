ddfc0c3a9fdafed1b7d30ecefff7bf37ccfba316
debonius <francky@tuta.io>
2023-06-18 08:15:44 +0200

    use unique file and clean

--- src/app/components/all-scores/all-scores.component.ts
+++ src/app/components/all-scores/all-scores.component.ts
@@ -5,7 +5,17 @@ import {GamesResponse} from "../../API/games-response";
 
 @Component({
   selector: 'app-all-scores',
-  templateUrl: './all-scores.component.html',
+  template: `
+    <app-header-scores></app-header-scores>
+    <h1 class="section-heading">Game scores</h1>
+    <mat-card class="card--rounded results">
+        <mat-card-content>
+            <ul class="results__list">
+                <app-latest-score-item *ngFor="let score of (latestScoresItems | async)?.data" [score]="score"></app-latest-score-item>
+            </ul>
+        </mat-card-content>
+    </mat-card>
+  `,
   styleUrls: ['./all-scores.component.scss']
 })
 export class AllScoresComponent implements OnInit {
