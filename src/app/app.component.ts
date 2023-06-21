112bc85621b16f76a26d3f51c4441558f1504328
debonius <francky@tuta.io>
2023-06-21 21:30:37 +0200

    fix app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <router-outlet></router-outlet>
+    <app-footer></app-footer>
+    <app-navigation></app-navigation>
+  `,
   styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
-  title = 'serwis-nba';
-  newsUrl = 'https://www.nba.com/news';
+  
 }
-
-// export class IconOverviewExample {}
-
