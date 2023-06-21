99efab90ae3465cea0ee18f1beb842ea23ac1480
debonius <francky@tuta.io>
2023-06-21 21:30:18 +0200

    fix markup

--- src/app/components/common/navigation/navigation.component.ts
+++ src/app/components/common/navigation/navigation.component.ts
@@ -4,26 +4,26 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-navigation',
   template: `
     <nav class="main-nav">
-      <button [routerLink] = "['']" mat-icon-button color="accent" aria-label="Go to Home">
+      <div [routerLink] = "['']" mat-icon-button color="accent" aria-label="Go to Home">
           <mat-icon>home</mat-icon>
           <span class="main-nav__label">Home</span>
-      </button>
-      <button [routerLink]= "['/search']" mat-icon-button color="primary" aria-label="Go to search page">
+      </div>
+      <div [routerLink]= "['/search']" mat-icon-button color="primary" aria-label="Go to search page">
           <mat-icon>search</mat-icon>
           <span class="main-nav__label">Search</span>
-      </button>
-      <button [routerLink] = "['/teams']" mat-icon-button color="primary" aria-label="Go to Teams page">
+      </div>
+      <div [routerLink] = "['/teams']" mat-icon-button color="primary" aria-label="Go to Teams page">
           <mat-icon>groups</mat-icon>
           <span class="main-nav__label">Teams</span>
-      </button>
-      <button [routerLink] = "['/scores']" mat-icon-button color="primary" aria-label="Go to Scoreboard">
+      </div>
+      <div [routerLink] = "['/scores']" mat-icon-button color="primary" aria-label="Go to Scoreboard">
           <mat-icon>sports_basketball</mat-icon>
           <span class="main-nav__label">Scores</span>
-      </button>
-      <button [routerLink]="['/news']" mat-icon-button color="primary" aria-label="Read News">
+      </div>
+      <div [routerLink]="['/news']" mat-icon-button color="primary" aria-label="Read News">
           <mat-icon>newspaper</mat-icon>
           <span class="main-nav__label">News</span>
-      </button>
+      </div>
     </nav>
   `,
   styleUrls: ['./navigation.component.scss']
