36cd52d787a17398efb406a890fa8c252984763f
debonius <francky@tuta.io>
2023-06-22 17:52:57 +0200

    fixed get scores

--- src/app/views/game-scores/game-scores.component.ts
+++ src/app/views/game-scores/game-scores.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-game-scores',
   template: `
     <app-header [heading]="heading"></app-header>
+    <app-latest-scores></app-latest-scores>
   `,
   styleUrls: ['./game-scores.component.scss']
 })
