f3460e604f9b49e0a6ba9bfeca630a418748a9e3
debonius <francky@tuta.io>
2023-06-22 09:01:21 +0200

    change order of nav elements

--- src/app/components/common/navigation/navigation.component.ts
+++ src/app/components/common/navigation/navigation.component.ts
@@ -8,14 +8,6 @@ import { Component, OnInit } from '@angular/core';
           <mat-icon>home</mat-icon>
           <span class="main-nav__label">Home</span>
       </div>
-      <div [routerLink]= "['/search']" mat-icon-button color="primary" aria-label="Go to search page">
-          <mat-icon>search</mat-icon>
-          <span class="main-nav__label">Search</span>
-      </div>
-      <div [routerLink] = "['/teams']" mat-icon-button color="primary" aria-label="Go to Teams page">
-          <mat-icon>groups</mat-icon>
-          <span class="main-nav__label">Teams</span>
-      </div>
       <div [routerLink] = "['/scores']" mat-icon-button color="primary" aria-label="Go to Scoreboard">
           <mat-icon>sports_basketball</mat-icon>
           <span class="main-nav__label">Scores</span>
@@ -23,6 +15,14 @@ import { Component, OnInit } from '@angular/core';
       <div [routerLink]="['/news']" mat-icon-button color="primary" aria-label="Read News">
           <mat-icon>newspaper</mat-icon>
           <span class="main-nav__label">News</span>
+      </div>
+      <div [routerLink]= "['/search']" mat-icon-button color="primary" aria-label="Go to search page">
+          <mat-icon>search</mat-icon>
+          <span class="main-nav__label">Search</span>
+      </div>
+      <div [routerLink] = "['/teams']" mat-icon-button color="primary" aria-label="Go to Teams page">
+          <mat-icon>groups</mat-icon>
+          <span class="main-nav__label">Teams</span>
       </div>
     </nav>
   `,
