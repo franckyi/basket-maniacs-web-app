680e778c097ff085b8b984702b4b41f4a1fd66e8
debonius <francky@tuta.io>
2023-06-24 18:53:02 +0200

    add news

--- src/app/components/news-list/news-list.component.ts
+++ src/app/components/news-list/news-list.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ApiService } from 'src/app/API/api.service';
-import {Observable} from "rxjs";
+import { Observable } from "rxjs";
 import { News } from 'src/app/API/news';
 
 @Component({
   selector: 'app-news-list',
   template: `
+    <h1 class="section-heading">News</h1>
     <mat-list>
-      <mat-list-item *ngFor="let item of (newsList | async)">
+      <mat-list-item *ngFor="let item of (newsList | async) | slice:0:perPage">
         <span matListItemTitle>{{ item.title }}</span>
         <a [href]="item.url" target="_blank" rel="nofollow">Read on nba.com</a>
       </mat-list-item>
@@ -15,14 +16,19 @@ import { News } from 'src/app/API/news';
   `,
   styleUrls: ['./news-list.component.scss']
 })
+
 export class NewsListComponent implements OnInit {
+  @Input() perPage?: number;
   newsList: Observable<News[]>;
   
   constructor(private api: ApiService) {
     this.newsList = api.getNews();
+    console.log('news perPage:', this.perPage);
   }
 
   ngOnInit(): void {
+    console.log('onInit');
+    console.log('news perPage:', this.perPage);
   }
 
 }
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -4,15 +4,17 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-home',
   template: `
     <app-header [heading]="heading"></app-header>
-    <app-latest-scores [perPage]="perPage"></app-latest-scores>
-    <!-- <app-latest-news></app-latest-news> -->
+    <app-news-list [perPage]="NewsPerPage"></app-news-list>
+    <button [routerLink]="['/news']" mat-flat-button color="primary">More news</button>
+    <app-latest-scores [perPage]="gamesPerPage"></app-latest-scores>
     <button [routerLink]="['/scores']" mat-flat-button color="primary">All game scores</button>
   `,
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   heading: string = 'Check last NBA scores, stay update about teams and players.';
-  perPage: number = 4;
+  gamesPerPage: number = 4;
+  NewsPerPage: number = 3;
   
   constructor() { }
 
