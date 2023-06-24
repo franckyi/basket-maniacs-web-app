d026ec13ccbb0ed3310a7e60fbef78b73411e96d
debonius <francky@tuta.io>
2023-06-24 18:21:31 +0200

    use unique file for  component

--- src/app/components/teams-list/all-teams-item/all-teams-item.component.ts
+++ src/app/components/teams-list/all-teams-item/all-teams-item.component.ts
@@ -3,7 +3,16 @@ import {Team} from "../../../API/Team";
 
 @Component({
   selector: 'app-all-teams-item',
-  templateUrl: './all-teams-item.component.html',
+  template: `
+    <li class="results__item team">
+      <span class="team__score">{{ team?.full_name | uppercase }} ({{ team?.abbreviation }}), {{ team?.division }} division</span>
+      <img
+        class="team__logo"
+        src="../../assets/img/team-logos/{{ team?.abbreviation }}.svg"
+        [alt]="team?.full_name + ' logo'"
+      >
+    </li>
+  `,
   styleUrls: ['./all-teams-item.component.scss']
 })
 export class AllTeamsItemComponent {
