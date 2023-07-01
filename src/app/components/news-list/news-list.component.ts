180853f4b2c01b6db35c6709b86aa14eb974e2b1
Francky Iside <francky@tuta.io>
2023-07-01 21:51:44 +0200

    add source providers

--- src/app/components/news-list/news-list.component.ts
+++ src/app/components/news-list/news-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { ApiService } from 'src/app/API/api.service';
 import { Observable } from "rxjs";
 import { News } from 'src/app/API/news';
@@ -7,6 +7,18 @@ import { News } from 'src/app/API/news';
   selector: 'app-news-list',
   template: `
     <h2 class="section-heading">Last news</h2>
+
+    <label id="radio-group-label">Pick a news provider</label>
+    <mat-radio-group
+      aria-labelledby="radio-group-label"
+      class="radio-group"
+      (change)="changeProvider($event)"
+      [(ngModel)]="selectedSource">
+      <mat-radio-button class="radio-button" *ngFor="let source of sources" [value]="source">
+        {{source}}
+      </mat-radio-button>
+    </mat-radio-group>
+    <div>Your currently viewing news from <strong>{{selectedSource}}</strong></div>
 
     <mat-list class="results__list">
       <a [href]="item.url" *ngFor="let item of (newsList | async) | slice:0:perPage" target="_blank" rel="nofollow">
@@ -18,22 +30,31 @@ import { News } from 'src/app/API/news';
   `,
   styleUrls: ['./news-list.component.scss']
 })
-export class NewsListComponent implements OnInit {
+export class NewsListComponent implements OnInit, AfterViewInit {
 
   @Input() perPage?: number;
+
+  newsList?: Observable<News[]>;
+
+  sources: string[] = [ 'nba-canada', 'nba', 'bleacher-report', 'espn', 'slam' ]
+  selectedSource?: string;
   
-  newsList: Observable<News[]>;
   hidden = false;
 
-  toggleBadgeVisibility() {
-    this.hidden = !this.hidden;
-  }
-
   constructor(private api: ApiService) {
-    this.newsList = api.getNews();
+    
   }
 
   ngOnInit(): void {
+    this.newsList = this.api.getInitialNews();
+  }
+
+  changeProvider($event: any) {
+    this.selectedSource = $event.value;
+    this.newsList = this.api.getNews(this.selectedSource?? 'nba');
+  }
+
+  ngAfterViewInit(): void {
 
   }
 
