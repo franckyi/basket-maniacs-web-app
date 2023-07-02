dfa7a73fe86391b40d3ab14103d81659de1f3386
Francky Iside <francky@tuta.io>
2023-07-02 18:25:16 +0200

    correct html semantic

--- src/app/components/common/navigation/navigation.component.ts
+++ src/app/components/common/navigation/navigation.component.ts
@@ -6,30 +6,30 @@ import { Router, NavigationStart, Event as NavigationEvent } from '@angular/rout
   template: `
     <nav class="main-nav">
 
-      <div [routerLink] = "['']" class="main-nav__link" mat-icon-button aria-label="Go to Home">
+      <a [routerLink] = "['']" class="main-nav__link" mat-icon-button aria-label="Go to Home">
           <mat-icon>home</mat-icon>
           <span class="main-nav__label">Home</span>
-      </div>
+      </a>
 
-      <div [routerLink]= "['/search']" mat-icon-button class="main-nav__link" aria-label="Go to search page">
+      <a [routerLink]= "['/search']" mat-icon-button class="main-nav__link" aria-label="Go to search page">
           <mat-icon>search</mat-icon>
           <span class="main-nav__label">Search</span>
-      </div>
+      </a>
 
-      <div [routerLink]="['/news']" mat-icon-button class="main-nav__link" aria-label="Read News">
+      <a [routerLink]="['/news']" mat-icon-button class="main-nav__link" aria-label="Read News">
           <mat-icon>newspaper</mat-icon>
           <span class="main-nav__label">News</span>
-      </div>
+      </a>
 
-      <div [routerLink] = "['/games']" mat-icon-button class="main-nav__link" aria-label="Go to Scoreboard">
+      <a [routerLink] = "['/games']" mat-icon-button class="main-nav__link" aria-label="Go to Scoreboard">
           <mat-icon>sports_basketball</mat-icon>
           <span class="main-nav__label">Scores</span>
-      </div>
+      </a>
 
-      <div [routerLink] = "['/teams']" mat-icon-button class="main-nav__link" aria-label="Go to Teams page">
+      <a [routerLink] = "['/teams']" mat-icon-button class="main-nav__link" aria-label="Go to Teams page">
           <mat-icon>groups</mat-icon>
           <span class="main-nav__label">Teams</span>
-      </div>
+      </a>
 
     </nav>
   `,
