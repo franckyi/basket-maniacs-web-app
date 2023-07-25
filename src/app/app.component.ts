f8c4c7c9dce5a7f04a4ff6172ada2c8f5679b59c
Francky Iside <francky@tuta.io>
2023-07-25 18:24:00 +0200

    fix toolbar and spacing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-toolbar></app-toolbar>
-    <router-outlet></router-outlet>
+    <main>
+      <app-toolbar class="toolbar"></app-toolbar>
+      <router-outlet></router-outlet>
+    </main>
   `,
   styleUrls: ['./app.component.scss']
 })
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -8,15 +8,17 @@ import { GamesService } from 'src/app/services/games.service';
   template: `
     <app-header [introText]="introText"></app-header>
     <app-logo [logoWidth]="70" [logoHeight]="70"></app-logo>
+
     <h2>Latest news</h2>
     <app-news-list class="home" [perPage]="NewsPerPage"></app-news-list>
     <button [routerLink]="['/news']" mat-flat-button color="primary">More news</button>
+
     <h2>Latest game results</h2>
     <mat-list class="results__list home">
       <app-game-list-item *ngFor="let game of (lastGames | async)?.data" [game]="game"></app-game-list-item>
     </mat-list>
-
     <button [routerLink]="['/games']" mat-flat-button color="primary">All game results</button>
+    
     <app-footer></app-footer>
   `,
   styleUrls: ['./home.component.scss']
