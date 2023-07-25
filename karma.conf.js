c788c7d022b91d33a942f5a76016173ebe9d9bee
Francky Iside <francky@tuta.io>
2023-07-25 19:38:41 +0200

    update project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/nba-news'),
+      dir: require('path').join(__dirname, './coverage/basket-maniacs'),
       subdir: '.',
       reporters: [
         { type: 'html' },
